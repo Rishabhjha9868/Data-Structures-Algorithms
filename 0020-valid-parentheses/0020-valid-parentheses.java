3805f17c752adf72bc6647ff6221364d0a7b245c
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2026-07-21 23:47:47 +0530

    Time: 3 ms (86.86%), Space: 43.4 MB (41.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,17 +2,22 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
-                st.push(s.charAt(i));
+            char ch = s.charAt(i);
+            if(ch =='(' || ch == '{' || ch == '[') {
+                st.push(ch);
             } else {
                 if(st.isEmpty()) return false;
                 char temp = st.pop();
-                if((temp == '(' && s.charAt(i) != ')') || (temp == '[' && s.charAt(i) != ']') || (temp == '{' && s.charAt(i) != '}')) {
+                if((temp == '(' && ch != ')' ) || 
+                (temp == '{' && ch != '}') || 
+                (temp == '[' && ch != ']')) {
                     return false;
                 }
             }
         }
-        return st.isEmpty() ? true:false;
-        
+        if(!st.isEmpty() ) {
+            return false;
+        } 
+        return true;
     }
 }
